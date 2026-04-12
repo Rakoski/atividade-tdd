b53303f2024c69e63513c5caa364fd6847cecf15
Rakoski <mastrakoski@gmail.com>
2026-04-12 14:49:45 -0300

    refactor: utilizando da class Character para melhor legibilidade do código

--- atividade-tdd/src/main/java/br/com/PasswordValidator.java
+++ atividade-tdd/src/main/java/br/com/PasswordValidator.java
@@ -4,32 +4,21 @@ public class PasswordValidator {
 
     public boolean isValid(String password) {
         if (password == null) return false;
+
         if (password.length() < 8) return false;
+
+        if (password.contains(" ")) return false;
 
         boolean hasUpperChar = false;
         boolean hasLowerChar = false;
         boolean hasDigit = false;
-        boolean hasSpecial = false;
-        String specialCharacters = "!@#$%^&*";
 
-        char[] chars = password.toCharArray();
-
-        for (char c : chars) {
-            if (c == ' ') return false;
-            if (c >= 'A' && c <= 'Z') hasUpperChar = true;
-            else if (c >= 'a' && c <= 'z') hasLowerChar = true;
-            else if (c >= '0' && c <= '9') hasDigit = true;
-
-            else {
-                for (int j = 0; j < specialCharacters.length(); j++) {
-                    if (c == specialCharacters.charAt(j)) {
-                        hasSpecial = true;
-                        break;
-                    }
-                }
-            }
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) hasUpperChar = true;
+            else if (Character.isLowerCase(c)) hasLowerChar = true;
+            else if (Character.isDigit(c)) hasDigit = true;
         }
 
-        return hasUpperChar && hasLowerChar && hasDigit && hasSpecial;
+        return hasUpperChar && hasLowerChar && hasDigit;
     }
 }
